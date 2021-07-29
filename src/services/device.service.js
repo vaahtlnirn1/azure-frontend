a43a3f4fb7716db74372a04fefde066e95f5cd52
Marko Mattila <marko@avulias.fi>
2021-07-29 14:13:41 +0300

    Fixed bug on string literals

--- src/services/device.service.js
+++ src/services/device.service.js
@@ -9,7 +9,7 @@ const retrieveDevices = () => {
 
 const retrieveDevice = id => {
     // eslint-disable-next-line
-    return axios.get(API_URL + "devices/${id}", { headers: authHeader(), "Content-type": "application/json" });
+    return axios.get(API_URL + `device/${id}`, { headers: authHeader(), "Content-type": "application/json" });
 };
 
 const createDevice = data => {
@@ -18,12 +18,12 @@ const createDevice = data => {
 
 const updateDevice = (id, data) => {
     // eslint-disable-next-line
-    return axios.put(API_URL + "device/${id}", { headers: authHeader(), "Content-type": "application/json", data });
+    return axios.put(API_URL + `device/${id}`, { headers: authHeader(), "Content-type": "application/json", data });
 };
 
 const deleteDevice = id => {
     // eslint-disable-next-line
-    return axios.delete(API_URL + "device/${id}", { headers: authHeader(), "Content-type": "application/json" });
+    return axios.delete(API_URL + `device/${id}`, { headers: authHeader(), "Content-type": "application/json" });
 };
 
 const deleteAllDevices = () => {
@@ -32,7 +32,7 @@ const deleteAllDevices = () => {
 
 const findDevicesByTitle = title => {
     // eslint-disable-next-line
-    return axios.get(API_URL + "devices?title=${title}", { headers: authHeader(), "Content-type": "application/json" });
+    return axios.get(API_URL + `devices?title=${title}`, { headers: authHeader(), "Content-type": "application/json" });
 };
 
 const DeviceService = {
